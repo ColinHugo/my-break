6e2d373dab290162dc64d211feb42bdcfd4eb50a
Hugo Colin <colin.hugo.89@gmail.com>
2022-02-23 20:44:58 -0600

    Sehizo obligatorio el campo nombre de la coleccion visita

--- models/visita.js
+++ models/visita.js
@@ -4,7 +4,7 @@ const visitaSchema = mongoose.Schema( {
 
     nombre: {
         type: String,
-        required: true,
+        required: [ true, 'El nombre es obligatorio.' ],
         trim: true
     },
 
