152eb78aa52b93570ea4911e4885c545998302de
Hugo Colin <colin.hugo.89@gmail.com>
2022-03-31 11:41:01 -0600

    Se cambio el nombre del archivo subir-archivo a archivo

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,7 @@
 import * as dbValidators from './db-validators.js';
 import generarJWT from './generar-jwt.js';
 import generarUrlFotos from './generar-url-fotos.js';
-import * as archivo from './subir-archivo.js';
+import * as archivo from './archivo.js';
 
 export {
     archivo,
